211c9cde5e495723896c155518460740f8c8a335
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-01-08 23:25:25 +0300

    брах

--- Kuana.Bot/Modules/VoiceModule.cs
+++ Kuana.Bot/Modules/VoiceModule.cs
@@ -24,13 +24,11 @@ namespace Kuana.Bot.Modules
         private async Task SendAsync(IAudioClient client, string path)
         {
             // Create FFmpeg using the previous example
-            using (var ffmpeg = CreateStream(path))
-            using (var output = ffmpeg.StandardOutput.BaseStream)
-            using (var discord = client.CreatePCMStream(AudioApplication.Mixed))
-            {
-                try { await output.CopyToAsync(discord); }
-                finally { await discord.FlushAsync(); }
-            }
+            using var ffmpeg = CreateStream(path);
+            using var output = ffmpeg.StandardOutput.BaseStream;
+            using var discord = client.CreatePCMStream(AudioApplication.Mixed);
+            try { await output.CopyToAsync(discord); }
+            finally { await discord.FlushAsync(); }
         }
 
         private Process CreateStream(string path)
