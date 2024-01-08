66a502f25b6bfa4d6226384cb60069d8166078d6
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-01-08 21:15:55 +0300

    Запуск

--- Kuana.Bot/Program.cs
+++ Kuana.Bot/Program.cs
@@ -5,3 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 var serviceProvider = new ServiceCollection()
     .AddSingleton<IBot, KuanaBot>()
     .BuildServiceProvider();
+
+var bot = serviceProvider.GetService<IBot>()!;
+await bot.Run();
