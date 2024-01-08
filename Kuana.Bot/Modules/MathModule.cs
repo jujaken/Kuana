3fb31418f65701384c046e3bb9dddbfab7134cfc
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2024-01-08 23:42:54 +0300

    Забубенил прикольную штуку

---
+++ Kuana.Bot/Modules/MathModule.cs
@@ -0,0 +1,17 @@
+﻿
+using Discord.Interactions;
+using org.mariuszgromada.math.mxparser;
+
+namespace Kuana.Bot.Modules
+{
+    [Group("math", "sexymatica )0)")]
+    public class MathModule : ModuleBase
+    {
+        [SlashCommand("solve-expression", "solve the expression")]
+        public async Task SolveExpression(string expression)
+        {
+            var exp = new Expression(expression);
+            await RespondAsync(expression + " " + exp.calculate());
+        }
+    }
+}
